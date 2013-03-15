4ef5800fcc72850da6c47d9bd3c33ed79561237b
Patrick Connelly <patrick@deadlypenguin.com>
2013-03-15 17:01:37 -0400

    Adding bak and swp dirs to setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 DFILES=dotfiles
 
+mkdir -p $HOME/.swp $HOME/.bak
+
 sudo yum -y install vim-enhanced pyflakes python-pip fontforge
 sudo pip-python install dotfiles
+
 cd $HOME
 git clone git@github.com:pcon/my-dotfiles.git $DFILES
 cd $HOME/$DFILES
 git submodule init
 git submodule update
+
 cd $HOME
 cp $HOME/dotfiles/dotfilesrc $HOME/.dotfilesrc
 dotfiles --sync --force
